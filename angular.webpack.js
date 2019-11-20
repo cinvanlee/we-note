1cb7f4187bdfc6c624e630a38318348cf01f2c55
rmlzy <liuzhiyuan2018@gmail.com>
2019-11-20 21:13:05 +0800

    feat: Format code

--- angular.webpack.js
+++ angular.webpack.js
@@ -2,17 +2,21 @@
  * Custom angular webpack configuration
  */
 module.exports = (config, options) => {
-    config.target = 'electron-renderer';
+    config.target = "electron-renderer";
     if (options.customWebpackConfig.target) {
         config.target = options.customWebpackConfig.target;
     } else if (options.fileReplacements) {
         for (let fileReplacement of options.fileReplacements) {
-            if (fileReplacement.replace !== 'src/environments/environment.ts') {
+            if (fileReplacement.replace !== "src/environments/environment.ts") {
                 continue;
             }
 
-            let fileReplacementParts = fileReplacement['with'].split('.');
-            if (['dev', 'prod', 'test', 'electron-renderer'].indexOf(fileReplacementParts[1]) < 0) {
+            let fileReplacementParts = fileReplacement["with"].split(".");
+            if (
+                ["dev", "prod", "test", "electron-renderer"].indexOf(
+                    fileReplacementParts[1]
+                ) < 0
+            ) {
                 config.target = fileReplacementParts[1];
             }
             break;
@@ -20,4 +24,4 @@ module.exports = (config, options) => {
     }
 
     return config;
-}
+};
